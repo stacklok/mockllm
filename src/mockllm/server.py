9658abe97544cef1b1bf08d9a63e998cd2cd2793
Luke Hinds <luke@stacklok.com>
2025-02-16 11:51:39 +0000

    Use tiktoken for more token usages information

--- src/mockllm/server.py
+++ src/mockllm/server.py
@@ -1,7 +1,7 @@
 import logging
-import tiktoken
 from typing import AsyncGenerator, Union
 
+import tiktoken
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from pythonjsonlogger import jsonlogger
